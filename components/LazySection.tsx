13bd8df0988f132cf2a0147c2dbf84e2e7d2831d
Dafa Jaya Priyatna <dafajayapriyatna5@gmail.com>
2026-07-22 10:13:25 +0700

    Create LazySection.tsx

---
+++ components/LazySection.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+export default function LazySection({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const ref = useRef(null);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setVisible(true);
+          observer.disconnect();
+        }
+      },
+      {
+        threshold: 0.1,
+      }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div ref={ref}>
+      {visible ? children : <div style={{ height: "500px" }} />}
+    </div>
+  );
+}
